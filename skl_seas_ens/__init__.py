81c814a064aff65ed7c1be57ab2a4f090b0659e2
Max Gläser <maximilian.glaeser@hotmail.de>
2025-03-19 01:23:24 +0100

    fix __init__

--- skl_seas_ens/__init__.py
+++ skl_seas_ens/__init__.py
@@ -1,11 +1,10 @@
 # Authors: scikit-learn-contrib developers
 # License: BSD 3 clause
 
-from ._template import SeasonalClassifier
+from .seasonal_classifier import SeasonalClassifier
 from ._version import __version__
 
 __all__ = [
-    "TemplateEstimator",
     "SeasonalClassifier",
     "__version__",
 ]

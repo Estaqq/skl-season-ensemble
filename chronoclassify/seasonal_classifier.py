42296722f69df6c664679ec58d7aa16151d60a4f
Max Gläser <maximilian.glaeser@hotmail.de>
2025-03-24 14:22:04 +0100

    linting

--- chronoclassify/seasonal_classifier.py
+++ chronoclassify/seasonal_classifier.py
@@ -6,23 +6,22 @@ be fitted to data from different seasons.
 # Authors: Max Gläser
 # License: BSD 3 clause
 
+import math
+
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin, _fit_context
 from sklearn.utils.multiclass import check_classification_targets
-from sklearn.utils.validation import check_is_fitted, check_X_y
+from sklearn.utils.validation import check_is_fitted
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils.validation import validate_data
-#import sklearn.utils.validation as validation
-import math
-
-
 
 # Note that the mixin class should always be on the left of `BaseEstimator` to ensure
 # the MRO works as expected.
 class SeasonalClassifier(ClassifierMixin, BaseEstimator):
-    """An classifier, which manages a collection of base estimators of type base_model_class and trains 
-    and calls them depending on the value of the feature of the name or index passed in time_column.
+    """An classifier, which manages a collection of base estimators of type base_model_class and 
+    trains and calls them depending on the value of the feature of the name or index passed 
+    in time_column.
 
     Parameters 
     ----------
@@ -34,26 +33,36 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
     window_size : int, float, default=None
         The size of the windows into which the data is split.
     window_start : int, float, default=None
-        The lower bound for the range for which we expect data. If None, the minimum value occuring in the training data is used.
+        The lower bound for the range for which we expect data. If None, 
+        the minimum value occuring in the training data is used.
     window_end : int, float, default=None
-        The upper bound for the range for which we expect data. If None, the maximum value occuring in the training data is used.
+        The upper bound for the range for which we expect data. If None, 
+        the maximum value occuring in the training data is used.
     n_windows : int, default=10
-        The number of windows in which the data is split. If window_size is set, this parameter is ignored.
+        The number of windows in which the data is split. If window_size is set, 
+        this parameter is ignored.
     windows : list, default=None
-        A list of the boundaries of the windows. If set, window_size, window_start and window_end are ignored.
+        A list of the boundaries of the windows. If set, 
+        window_size, window_start and window_end are ignored.
     padding : int, float, default=105
-        The distance in time units away from some window for which data is still considered for training the classifier for that particular window.
+        The distance in time units away from some window for which data is 
+        still considered for training the classifier for that particular window.
     time_column : str, int, default=0
-        The index or name of the column in the input data that contains the time information based on which data is assigned to base classifiers.
+        The index or name of the column in the input data that contains the 
+        time information based on which data is assigned to base classifiers.
         Strings can only be passed, if the input data is a pandas DataFrame or Series.
-        When using SeasonalClassifier together with functions such as grid_search or cross_validate, either a numerical index must be provided,
-        or the argument col_names must also be provided, since those functions pass numpy arrays during their invocation of SeasonalClassifier.
+        When using SeasonalClassifier together with functions such as grid_search or 
+        cross_validate, either a numerical index must be provided,
+        or the argument col_names must also be provided, since those functions pass 
+        numpy arrays during their invocation of SeasonalClassifier.
     col_names : list, default=None
         The names of the columns in the input data. Only needed when the time_column is a string.
     drop_time_column : bool, default=True
-        Whether to drop the time column from the input data before passing it to the base classifiers. Ignored when the input data has only one feature.
+        Whether to drop the time column from the input data before passing it to 
+        the base classifiers. Ignored when the input data has only one feature.
     data_is_periodic : bool, default=True
-        Whether the data is periodic. If True, data is considered to be periodic and the windows are wrapped around the window_start and window_end values.
+        Whether the data is periodic. If True, data is considered to be periodic and the windows are 
+        wrapped around the window_start and window_end values.
 
     Attributes 
     ----------
@@ -94,9 +103,9 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
     }
 
     def __init__(
-            self, 
+            self,
             base_model_class = LogisticRegression,
-            base_model_args = None, 
+            base_model_args = None,
             window_size: int | float | None = None,
             window_start: int | float | None = None,
             window_end: int | float | None = None,
@@ -119,13 +128,13 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
         self.windows = windows
         self.padding = padding
         self.time_column = time_column
-        self.drop_time_column = drop_time_column   
+        self.drop_time_column = drop_time_column
         self.data_is_periodic = data_is_periodic
         self.col_names = col_names
         self.verbose = verbose
 
     def _set_up_windows(self):
-        if self.windows != None:
+        if self.windows is not None:
             self._internal_windows = self.windows
             assert len(self._internal_windows) > 1
         else:
@@ -138,7 +147,7 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
             if day <= self._window_start:
                 return self._window_start
             elif day >= self._window_end:
-                return self._window_end 
+                return self._window_end
             else:
                 return day
         return
@@ -153,15 +162,15 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
         if day == self._internal_windows[len(self._internal_windows) - 1]:
             return len(self._internal_windows) - 2
         raise ValueError("Value of time column is out of bounds.")
-    
+
     def _create_models(self):
-        if self.base_model_args == None:
+        if self.base_model_args is None:
             self._base_model_args = {}
         else:
             self._base_model_args = self.base_model_args
         self._models = []
         assert len(self._internal_windows) > 1
-        for i in range( len(self._internal_windows) -1 ):
+        for _ in range( len(self._internal_windows) -1 ):
             self._models.append(self.base_model_class(**self._base_model_args))
 
     def _select_rows(self, data, window_index):
@@ -177,18 +186,18 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
                 mask2 = (data[:,self._time_column] < self._window_start + (end - self._window_end))
                 mask = mask | mask2
         return mask
-    
+
     def _fit_base_models(self):
-        for i in range(len(self._models)):
+        for i, model in enumerate(self._models):
             assert self.n_features_in_ == self.X_.shape[1]
             selection = self._select_rows(self.X_, i)
-            if(self._drop_time_column):
-                self._models[i].fit(np.delete(self.X_[selection,:], self._time_column,axis = 1), self.y_[selection])
+            if self._drop_time_column :
+                model.fit(np.delete(self.X_[selection,:], self._time_column,axis = 1), self.y_[selection])
             else:
-                self._models[i].fit(self.X_[selection,:], self.y_[selection])
+                model.fit(self.X_[selection,:], self.y_[selection])
             if self.verbose:
-                print(f"Model {i} fitted with {len(self.X_[selection])} samples.")  
-    
+                print(f"Model {i} fitted with {len(self.X_[selection])} samples.")
+
     def _apply_appropriate_model(self, row, str_func = 'predict'):
         #Applies the function whose name is given in the string func of the appropriate model to a row of data.
         window = self._get_window(row[self._time_column])
@@ -200,7 +209,7 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
             raise AttributeError(f"The base model does not have the method '{str_func}'")
         func = getattr(model, str_func)
         return func(row)
-        
+
     @_fit_context(prefer_skip_nested_validation=True)
     def fit(self, X, y):
         """Fitting function of this classifier.
@@ -235,37 +244,37 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
 
 
         # preprocess parameters
-        if type(self.time_column) is str:
-            if ((type(X) is pd.DataFrame) or (type(X) is pd.Series)):
+        if isinstance(self.time_column, str):
+            if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series):
                 self._time_column = X.columns.get_loc(self.time_column) - 1
             elif self.col_names is not None:
                 self._time_column = self.col_names.get_loc(self.time_column)
-                assert (type(self._time_column) is int)
+                assert isinstance(self._time_column, int)
             else:
                 raise ValueError("SeasonalClassifier If the time_column is provided as a string, the column names must be provided as well.")
         else: #time_column is an integer
             self._time_column = self.time_column
 
         # Store the training data to predict later
-        if type(X) is pd.DataFrame or type(X) is pd.Series:
+        if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series):
             self.X_ = X.values
         else:
             self.X_ = X
-        if type(y) is pd.Series:
+        if isinstance(y, pd.Series):
             self.y_ = y.values
         else:
             self.y_ = y
         assert self.n_features_in_ == self.X_.shape[1], "Number of columns in X_ does not match n_features_in_"
 
-        if self.window_end == None:
+        if self.window_end is None:
             self._window_end = self.X_[:,self._time_column].max()
         else:
             self._window_end = self.window_end
-        if self.window_start == None:
+        if self.window_start is None:
             self._window_start = self.X_[:,self._time_column].min()
         else:
             self._window_start = self.window_start
-        if self.window_size == None:
+        if self.window_size is None:
             self._n_windows = self.n_windows
         else:
             self._n_windows = math.ceil((self._window_end - self._window_start) / self.window_size)
@@ -308,7 +317,8 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
         return prediction
 
     def predict_proba(self, X):
-        """Predict the probabilities of the input samples belonging to each class using the appropriate base model.
+        """Predict the probabilities of the input samples belonging to each class using the 
+        appropriate base model.
 
         Parameters
         ----------
@@ -327,7 +337,8 @@ class SeasonalClassifier(ClassifierMixin, BaseEstimator):
         return prediction
 
     def predict_log_proba(self, X):
-        """Predict the log-probabilities of the input samples belonging to each class using the appropriate base model.
+        """Predict the log-probabilities of the input samples belonging to each class 
+        using the appropriate base model.
 
         Parameters
         ----------
